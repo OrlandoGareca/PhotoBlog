3dc6e50e7e97cfef0b652b7a6921406d1f27e444
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2018-08-30 20:10:59 -0400

    design login

--- app/src/main/java/com/orlandogareca/photoblog/LoginActivity.java
+++ app/src/main/java/com/orlandogareca/photoblog/LoginActivity.java
@@ -1,18 +1,15 @@
 package com.orlandogareca.photoblog;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
 
     }
 }
