0e7ca20f09a56eb29b02e00db4bfdd14c446eaf2
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2018-09-05 22:38:46 -0400

    implement NavigationView Fragment

--- app/src/main/java/com/orlandogareca/photoblog/MainActivity.java
+++ app/src/main/java/com/orlandogareca/photoblog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.orlandogareca.photoblog;
 
 import android.content.Intent;
+import android.icu.text.Replaceable;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -28,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton addPostBtn;
 
+    private BottomNavigationView mainbottomNav;
+
+    private  HomeFragment homeFragment;
+    private NotificationFragment notificationFragment;
+    private AccountFragment accountFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +53,36 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mainToolbar);
 
         getSupportActionBar().setTitle("Photo Blog");
+
+        mainbottomNav = findViewById(R.id.mainBottomNav);
+
+        //FRAGMENTS
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        accountFragment = new AccountFragment();
+
+        mainbottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch ( item.getItemId()){
+                    case R.id.bottom_action_home :
+                        replaceFragment(homeFragment);
+                        return true;
+                    case R.id.bottom_action_account :
+                        replaceFragment(accountFragment);
+                        return true;
+                    case R.id.bottom_action_notif :
+                        replaceFragment(notificationFragment);
+                        return true;
+
+                        default:
+                            return false;
+
+                }
+            }
+        });
+
 
         addPostBtn = findViewById(R.id.add_post_btn);
         addPostBtn.setOnClickListener(new View.OnClickListener() {
@@ -131,6 +171,15 @@ public class MainActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
         finish();
+    }
+
+    private void  replaceFragment(Fragment fragment){
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container,fragment);
+        fragmentTransaction.commit();
+
+
     }
 
 }
